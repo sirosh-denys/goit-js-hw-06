9db4bd5efe28ccc9f6d777ec08269770c815d641
Denis <sirosh.denys@gmail.com>
2023-03-24 11:54:47 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngredient = document.querySelector(`#ingredients`);
+
+const liArray = [];
+
+for (const ingredient of ingredients) {
+	const liEl = document.createElement("li");
+	liEl.textContent = `${ingredient}`;
+	liEl.classList.add(`item`);
+
+	liArray.push(liEl);	
+}
+
+listOfIngredient.append(...liArray);
+
