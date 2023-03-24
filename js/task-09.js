683a14d61c15c92be9e3a225edb12aaa64355dcc
Denis <sirosh.denys@gmail.com>
2023-03-24 13:15:45 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+	span: document.querySelector(`.color`),
+	button: document.querySelector(`.change-color`),
+};
+
+const clickHandle = () => {
+	const newColor = getRandomHexColor();
+	console.log(newColor);
+
+	document.body.style.backgroundColor = `${newColor}`;
+	refs.span.textContent = newColor;
+};
+
+refs.button.addEventListener(`click`, clickHandle);
