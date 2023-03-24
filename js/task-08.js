06d7a2ccf4aba6cb21a81967f40009fe13ad4dc0
Denis <sirosh.denys@gmail.com>
2023-03-24 12:55:52 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const refs = {
+	form: document.querySelector(`.login-form`),
+};
 
+const submitHandler = event => {
+	event.preventDefault();
+	
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (email.value === "" || password.value === "") {
+		return console.log("Всі поля мають бути заповнені!");
+	}
+
+	const objectEl = {
+		email: email.value,
+		password: password.value,
+	};
+
+	console.log(objectEl);
+
+	event.currentTarget.reset();
+};
+
+refs.form.addEventListener("submit", submitHandler);
